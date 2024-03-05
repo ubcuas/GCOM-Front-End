99762b14143374bf8a8f35e93b03469f10d9ad9e
Jim Geng <jimgeng2004@gmail.com>
2024-03-04 21:15:34 -0800

    install socketio

--- src/components/WaypointStatusCard.tsx
+++ src/components/WaypointStatusCard.tsx
@@ -1,5 +1,17 @@
 import InfoCard from "./InfoCard";
+import { io } from "socket.io-client";
 
 export default function WaypointStatus() {
+    const socket = io("http://localhost:1323");
+    socket.on("connect_error", (err: any) => {
+        // the reason of the error, for example "xhr poll error"
+        console.log(err.message);
+
+        // some additional description, for example the status code of the initial HTTP response
+        console.log(err.description);
+
+        // some additional context, for example the XMLHttpRequest object
+        console.log(err.context);
+    });
     return <InfoCard title="Waypoint"></InfoCard>;
 }
