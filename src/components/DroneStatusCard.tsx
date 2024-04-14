51eb70e976e6f8ac956275030d43b5a4d44a641c
Jim Geng <jimgeng2004@gmail.com>
2024-04-14 00:29:07 -0700

    add socket boilerplate code, require disabling strict mode since that does weird behaviour

--- src/components/DroneStatusCard.tsx
+++ src/components/DroneStatusCard.tsx
@@ -5,9 +5,22 @@ import PositionSection from "./DroneStatus/PositionSection";
 import SpeedSection from "./DroneStatus/SpeedSection";
 import TimeStamp from "./DroneStatus/TimeStamp";
 import MPSControlSection from "./DroneStatus/MPSControlSection";
+import { useEffect } from "react";
+import { socket } from "../api/socket";
 
 export default function DroneStatusCard() {
     const droneState = useAppSelector(selectAircraftStatus);
+
+    useEffect(() => {
+        socket.emit("telemetry_start");
+        socket.on("telemetry_data", (data) => {
+            console.log(data);
+        });
+        return () => {
+            socket.emit("telemetry_stop");
+            socket.off("telemetry_data");
+        };
+    }, []);
 
     return (
         <InfoCard title="Drone (wip)">
--- src/main.tsx
+++ src/main.tsx
@@ -9,9 +9,7 @@ import "@fontsource/inter/500.css";
 import "@fontsource/inter/700.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </React.StrictMode>,
+    <Provider store={store}>
+        <App />
+    </Provider>,
 );
