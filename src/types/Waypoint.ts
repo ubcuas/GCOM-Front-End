eb79fcec59a9392eff3efe683da2acdbe2cf75aa
MichaelDickinson20I <michael.dickinson3949@gmail.com>
2025-02-08 18:09:29 -0800

    removed waypoint fields

--- src/types/Waypoint.ts
+++ src/types/Waypoint.ts
@@ -18,11 +18,6 @@ export type Waypoint = {
     alt?: number;
     radius?: number;
     remarks?: string;
-    command?: string;
-    param1?: number;
-    param2?: number;
-    param3?: number;
-    param4?: number;
     order?: number;
 };
 
